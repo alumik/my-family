57e195e1d4d8abcf417f2d8a5fc4ad7ee9e8b31a
Zhong Zhenyu <nczzy1997@gmail.com>
2019-02-10 15:39:17 +0800

    开放 gii 权限

--- config/web.php
+++ config/web.php
@@ -68,7 +68,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 }
 
