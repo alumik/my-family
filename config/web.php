5d09b2612855ab58d761660beb71d7d499a86590
Zhong Zhenyu <nczzy1997@gmail.com>
2019-02-10 15:56:03 +0800

    打开 url 美化

--- config/web.php
+++ config/web.php
@@ -45,14 +45,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
