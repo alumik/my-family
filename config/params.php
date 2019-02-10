abe6abcecedc06efe4729d6dc5643d0f9ce1bc40
Zhong Zhenyu <nczzy1997@gmail.com>
2019-02-10 16:05:55 +0800

    调整更多信息页面

--- config/params.php
+++ config/params.php
@@ -2,4 +2,5 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'version' => '0.1.0',
 ];
--- views/site/about.php
+++ views/site/about.php
@@ -4,15 +4,15 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = '更多信息';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+    <div class="alert alert-info">
+        <?= nl2br(Html::encode('本网站作者：钟震宇。')) ?>
+    </div>
 
-    <code><?= __FILE__ ?></code>
+    <p>网站版本 <?= Yii::$app->params['version'] ?></p>
 </div>
