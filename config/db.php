098231d89d1d726cfcf1d04a510667f34428eeee
Zhong Zhenyu <nczzy1997@gmail.com>
2019-02-10 15:00:30 +0800

    首次提交

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=149.28.82.41;dbname=my_family',
+    'username' => 'root',
+    'password' => 'identify1',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
