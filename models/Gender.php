296fa11ba83c692a172dce365f5117801335ad8e
Zhong Zhenyu <nczzy1997@gmail.com>
2019-02-13 15:17:18 +0800

    重构 Gender Model

--- models/Gender.php
+++ models/Gender.php
@@ -10,8 +10,6 @@ use yii\helpers\ArrayHelper;
  *
  * @property int $id
  * @property string $name
- *
- * @property Person[] $people
  */
 class Gender extends \yii\db\ActiveRecord
 {
@@ -39,34 +37,11 @@ class Gender extends \yii\db\ActiveRecord
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'name' => 'Name',
-        ];
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getPeople()
-    {
-        return $this->hasMany(Person::className(), ['gender' => 'id']);
-    }
-
-    /**
      * @return array
      */
     public static function getGenderList()
     {
-        $result = [];
-        $list = Gender::find()->all();
-        if (!empty($list)) {
-            $result = ArrayHelper::map($list, 'id', 'name');
-        }
-        return $result;
+        $result = Gender::find()->all();
+        return ArrayHelper::map($result, 'id', 'name');
     }
 }
